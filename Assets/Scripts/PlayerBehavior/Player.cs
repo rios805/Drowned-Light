65e6f52fd41b2ddcc9cbdc839b0244c1f9434dcd
Rudy Cacho Hernandez <112025281+rudycacho@users.noreply.github.com>
2025-04-17 14:54:18 -0700

    polished player even further and polished enemy ai as well

--- Assets/Scripts/PlayerBehavior/Player.cs
+++ Assets/Scripts/PlayerBehavior/Player.cs
@@ -7,21 +7,30 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    
     // Singleton 
     public static Player Instance{ get; private set; }
+    
+    // Events
+    public EventHandler OnPlayerHealthChanged;
+    public EventHandler OnPlayerStaminaChanged;
+    public EventHandler OnPlayerSanityChanged;
+    public EventHandler OnPlayerKilled;
+    
     
     // Editable values
     private Vector3 playerVelocity;
     [Header("Movement Settings")]
     [SerializeField] private float defaultPlayerSpeed = 2.0f;
     [SerializeField] private float sprintSpeed = 6f;
+    [SerializeField] private float crouchSpeed = 1.0f;
     [SerializeField] private float gravityValue = -9.81f;
     [SerializeField] private Transform playerTopPoint;
     
     [Header("Player Settings")]
     [SerializeField] private float stamina = 100f;
     [SerializeField] private float sanity = 100f;
-    [SerializeField] private float health = 100f;
+    [SerializeField] private int health = 100;
     
     [Header("Camera Settings + Input")]
     [SerializeField]private CinemachineCamera virtualCamera;
@@ -29,11 +38,22 @@ public class Player : MonoBehaviour
     [SerializeField]private InputManager inputManager;
     [SerializeField]private float mainFOV = 60f;
     
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private List<AudioClip> footstepClips;
+    [SerializeField] private List<AudioClip> damageClips;
+    [SerializeField] private float walkStepRate = 0.5f;
+    [SerializeField] private float sprintStepRate = 0.3f;
+    [SerializeField] private float crouchStepRate = 0.8f;
+
+    private float footstepTimer = .5f;
+    private bool isMoving;
+    
     // Needed variables
     private bool groundedPlayer, isCrouched, isSprinting;
     private float playerSpeed,playerTargetHeight, targetFOV;
     private CharacterController controller;
-    private Vector3 playerTargetCenter;
+    private Vector3 playerTargetCenter, lastPosition, currentVelocity;
     private Transform cameraTransform;
     private float cameraOffset = 0.5f;
 
@@ -49,6 +69,7 @@ public class Player : MonoBehaviour
         cameraTransform = Camera.main.transform;
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        footstepTimer = walkStepRate;
     }
     void Update()
     {
@@ -67,16 +88,18 @@ public class Player : MonoBehaviour
         // Check if player is sprinting
         if (inputManager.PlayerInput_Sprint() && stamina > 0f && !isCrouched) {
             isSprinting = true;
-            playerSpeed = sprintSpeed;
             stamina -= Time.deltaTime * 20f;
             if (stamina < 0.1f) {
+                stamina = Mathf.Clamp(stamina, 0f, 100f);
                 isSprinting = false;
             }
+            OnPlayerStaminaChanged?.Invoke(this, EventArgs.Empty);
         } else {
             isSprinting = false;
-            playerSpeed = defaultPlayerSpeed;
             if (stamina < 100f) {
                 stamina += Time.deltaTime * 5f;
+                stamina = Mathf.Clamp(stamina, 0f, 100f);
+                OnPlayerStaminaChanged?.Invoke(this, EventArgs.Empty);
             }
         }
         
@@ -107,29 +130,75 @@ public class Player : MonoBehaviour
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
-        // Change FOV based off if player is standing, sprinting, or crouching
+        // Change FOV and speed based off if player is standing, sprinting, or crouching
         if (isSprinting || isCrouched) {
             if (isSprinting) {
                 targetFOV = 70;
+                playerSpeed = sprintSpeed;
             }
             else {
                 targetFOV = 50;
+                playerSpeed = crouchSpeed;
             }
         } else {
             targetFOV = mainFOV;
+            playerSpeed = defaultPlayerSpeed;
+            
         }
         // Set camera height and FOV
         controller.height = Mathf.Lerp(controller.height, playerTargetHeight, Time.deltaTime * 5f);
         controller.center = Vector3.Lerp(controller.center, playerTargetCenter, Time.deltaTime * 5f);
         cameraFollowTransform.localPosition = Vector3.Lerp(cameraFollowTransform.localPosition, new Vector3(cameraFollowTransform.localPosition.x,playerTargetCenter.y + cameraOffset,cameraFollowTransform.localPosition.z) , Time.deltaTime * 5f);
         virtualCamera.Lens.FieldOfView = Mathf.Lerp(virtualCamera.Lens.FieldOfView, targetFOV, Time.deltaTime * 3f);
-    }
-
-    public void TakeDamage(float damage) {
-        health -= damage;
-        if (health <= 0f) {
-            Destroy(gameObject);
+        
+        currentVelocity = (transform.position - lastPosition) / Time.deltaTime;
+        lastPosition = transform.position;
+        isMoving = controller.isGrounded && currentVelocity.magnitude > .1f;
+        if (isMoving) {
+            footstepTimer -= Time.deltaTime;
+            if (footstepTimer <= 0f) {
+                if (isCrouched)
+                    footstepTimer = crouchStepRate;
+                else if (isSprinting)
+                    footstepTimer = sprintStepRate;
+                else
+                    footstepTimer = walkStepRate;
+                PlayFootstep();
+            }
         }
     }
+
+    public void TakeDamage(int damage) {
+        if (damageClips.Count > 0) {
+            int index = UnityEngine.Random.Range(0, damageClips.Count);
+            audioSource.PlayOneShot(damageClips[index]);
+        }
+        
+        health -= damage;
+        if (health <= 0f) {
+            OnPlayerKilled?.Invoke(this, EventArgs.Empty);
+            Time.timeScale = 0f;
+        }
+        OnPlayerHealthChanged?.Invoke(this, EventArgs.Empty);
+    }
     
+    private void PlayFootstep()
+    {
+        audioSource.pitch = UnityEngine.Random.Range(0.95f, 1.05f);
+        if (footstepClips.Count == 0) return;
+        int index = UnityEngine.Random.Range(0, footstepClips.Count);
+        audioSource.PlayOneShot(footstepClips[index]);
+    }
+
+    public int GetHealth() {
+        return health;
+    }
+
+    public float GetStamina() {
+        return stamina;
+    }
+
+    public float GetSanity() {
+        return sanity;
+    }
 }
