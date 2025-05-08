94ff45a2f6b375717a3fb72300e8dd2e98f171bf
Rudy Cacho Hernandez <112025281+rudycacho@users.noreply.github.com>
2025-05-08 07:36:06 -0700

    added notes system and other minor changes

--- Assets/Scripts/Obstacles/DoorInteract.cs
+++ Assets/Scripts/Obstacles/DoorInteract.cs
@@ -2,40 +2,31 @@ using UnityEngine;
 
 public class DoorInteract : MonoBehaviour, IInteractbleItem
 {
-    [Header("Key + Range Settings")]
     [SerializeField] private KeyItemSO requiredKey;
-    [Tooltip("How close you must be to unlock/open this door")]
-    [SerializeField] private float interactRange = 3f;
-
     private DoorController door;
-    private Transform playerT;
 
     private void Awake()
     {
         door = GetComponent<DoorController>();
         if (door == null)
-            Debug.LogWarning("DoorController not found on this object!");
-
-        if (Player.Instance != null)
-            playerT = Player.Instance.transform;
+            UnityEngine.Debug.LogWarning("DoorController not found on this object!");
     }
-
+    
     public void OnPlayerInteract()
     {
-        // 1) are we still within range?
-        if (playerT == null ||
-            Vector3.Distance(playerT.position, transform.position) > interactRange)
-        {
-            Debug.Log("Too far from door to interact.");
-            return;
-        }
+        Inventory inv = Player.Instance.GetComponent<Inventory>();
 
-        // 2) do we have the right key?
-        var inv = Player.Instance.GetComponent<Inventory>();
         if (inv != null && inv.HasItem(requiredKey))
         {
             Debug.Log("Correct key used. Door unlocked!");
-            door?.OpenDoor();
+            if (door != null)
+            {
+                door.OpenDoor();
+            }
+            else
+            {
+                Debug.LogWarning("DoorController not assigned");
+            }   
         }
         else
         {
@@ -43,8 +34,12 @@ public class DoorInteract : MonoBehaviour, IInteractbleItem
         }
     }
 
-    // unused, but required by the interface
-    public void OnPlayerInteractAlternate() { }
-    public void HasOwner() { }
+    public void OnPlayerInteractAlternate()
+    {
+    }
+
+    public void HasOwner()
+    {
+    }
 }
 
--- Assets/Scripts/ScriptableObjects/DoorAnimator.cs
+++ Assets/Scripts/ScriptableObjects/DoorAnimator.cs
@@ -3,23 +3,16 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class DoorAnimator : MonoBehaviour
 {
-    public enum OpenDirection {Down, Up, Side}
+    public enum OpenDirection {Down, Up}
 
     public OpenDirection direction = OpenDirection.Down; 
-
-    // public enum CloseDirection {Down, Side}
-
-    // public CloseDirection closing = CloseDirection.Down; 
     
     public Animator animator;
 
     // caching the hash for efficiency
     private static readonly int OpenDownHash = Animator.StringToHash("OpenDown");
     private static readonly int OpenUpHash = Animator.StringToHash("OpenUp");
-    // private static readonly int OpenSideHash = Animator.StringToHash("OpenSide");
     private static readonly int CloseDownHash = Animator.StringToHash("CloseDown");
-    // private static readonly int CloseSideHash = Animator.StringToHash("CloseSide");
-
 
     void Awake()
     {
@@ -45,24 +38,11 @@ public class DoorAnimator : MonoBehaviour
             case OpenDirection.Up:
                 animator.SetTrigger(OpenUpHash);
                 break; 
-            // case OpenDirection.Side:
-            //     animator.SetTrigger(OpenSideHash);
-            //     break;
         }
     }
 
     public void CloseDoor()
     {
         animator.SetTrigger(CloseDownHash);
-        // switch(closing)
-        // {
-        //     case CloseDirection.Down:
-        //         animator.SetTrigger(CloseDownHash);
-        //         break;
-            
-        //     case CloseDirection.Side:
-        //         animator.SetTrigger(CloseSideHash);
-        //         break;
-        // }
     }
 }
